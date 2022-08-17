686234368c1b919bb340b167951d1e9daeb89a86
dannyputra <dannyptr21@gmail.com>
2022-08-17 10:14:53 +0700

    add all file

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import NFTCards from "./NFTCards";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+export {HomeHeader, NFTCards, FocusedStatusBar, CircleButton, RectButton, DetailsBid, DetailsDesc, SubInfo}
